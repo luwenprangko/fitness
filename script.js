06dd14ce233618783d660edd5918301f16c7d8f9
luwenprangko <louienfrancoaxalan@gmail.com>
2025-04-04 15:12:04 +0800

    Update

--- script.js
+++ script.js
@@ -22,4 +22,51 @@ tabs.forEach((tab) => {
 });
 
 // Set default active tab
-tabs[0].click();
+tabs[1].click();
+
+var map = new maplibregl.Map({
+	container: "map",
+	style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json", // stylesheet location
+	center: [-74.5, 40], // starting position [lng, lat]
+	zoom: 3, // starting zoom
+	attributionControl: false, // disables the attribution
+});
+
+// Add zoom and rotation controls to the map
+map.addControl(new maplibregl.NavigationControl(), "top-right");
+
+// Add geolocation control to the map
+map.addControl(
+	new maplibregl.GeolocateControl({
+		positionOptions: {
+			enableHighAccuracy: true,
+		},
+		trackUserLocation: true, // auto-follow user's location
+		showUserHeading: true, // show device direction arrow if available
+	}),
+	"top-right"
+);
+
+// Optional: center map on user location once loaded
+map.on("load", () => {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+			function (position) {
+				const userLng = position.coords.longitude;
+				const userLat = position.coords.latitude;
+				map.setCenter([userLng, userLat]);
+				map.setZoom(12);
+			},
+			function (error) {
+				console.warn("Geolocation failed or denied:", error.message);
+			}
+		);
+	}
+});
+
+// map.on("style.load", () => {
+// 	// Set projection to globe
+// 	map.setProjection({
+// 		type: "globe",
+// 	});
+// });
